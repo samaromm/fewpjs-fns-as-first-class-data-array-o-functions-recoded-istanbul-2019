4b160abda3eb664010fd457c2236fa3346841c68
Samar Omar <samaromm98@gmail.com>
2020-01-22 21:06:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function walkToPark(dogName, dogBreed) {
   console.log(str);
   return str;
 }
-function (dogName, dogBreed) {
+function throwFrisbee(dogName, dogBreed) {
   let str=`Throw the firsbee for ${dogName} the ${dogBreed}`;
   console.log(str);
   return str;
