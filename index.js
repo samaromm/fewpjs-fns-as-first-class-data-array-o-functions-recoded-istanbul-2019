01ac0c4041879d7117806fe6ac79d5fdfc5e0d6c
Samar Omar <samaromm98@gmail.com>
2020-01-22 21:00:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function wakeDog(dogName, dogBreed) {
+  let str=`Wake ${dogName} the ${dogBreed}`;
+  console.log(str);
+  return str;
+}
+function leashDog(dogName, dogBreed) {
+  let str=`Leash ${dogName} the ${dogBreed}`;
+  console.log(str);
+  return str;
+}
+function walkToPark(dogName, dogBreed) {
+  let str=`walk to the park with ${dogName} the ${dogBreed}`;
+  console.log(str);
+  return str;
+}
+function throwFirsbee(dogName, dogBreed) {
+  let str=`throw the firsbee for ${dogName} the ${dogBreed}`;
+  console.log(str);
+  return str;
+}
+function walkHome(dogName, dogBreed) {
+  let str=`walk home with ${dogName} the ${dogBreed}`;
+  console.log(str);
+  return str;
+}
+function unleashDog(dogName, dogBreed) {
+  let str=`Unleash ${dogName} the ${dogBreed}`;
+  console.log(str);
+  return str;
+}
+
+let routine =[wakeDog(),leashDog(), walkToPark(), throwFrisbee(), walkHome(), unleashDog()];
+
+function exerciseDog(dogName, dogBreed){
+  
+}
